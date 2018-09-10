58983a9136e0f4cef1e6d210b78add3015989e7b
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-09-10 21:32:05 +0200

    uprawnienia

--- Permissions.php
+++ Permissions.php
@@ -13,17 +13,48 @@ use Core\DB;
 
 class Permissions
 {
-    protected $data=[];
-public function __construct(int $userId)
-{
-    $this->load($userId);
-}
+    protected $data = [];
+
+    public function __construct(int $userId)
+    {
+        $this->load($userId);
+    }
 
     private function load(int $userId)
     {
-        $data=DB::get("SELECT * FROM user_permission up WHERE id_user = ?",[$userId]);
-        foreach ($data as $row){
-            $this->data[$row['name']]=1;
+        $data = DB::get("SELECT * FROM user_permission up WHERE id_user = ?", [$userId]);
+        foreach ($data as $row) {
+            $this->data[$row['name']] = 1;
         }
+    }
+
+    static function readStructure()
+    {
+        $data = [];
+        $groups = [];
+        $modules = scandir(__DIR__.'/../');
+        foreach ($modules as $module) {
+            if ($module == '.' || $module == '..') {
+                continue;
+            }
+            $filename = __DIR__.'/../'.$module.'/permissions.xml';
+            if (is_file($filename)) {
+                $xml = simplexml_load_string(file_get_contents($filename));
+                foreach ($xml->group as $group) {
+                    $groups[$group->name->__toString()]=$group;
+                    foreach ($group->permission as $permission) {
+                        $data[$group->name->__toString()][$permission->name->__toString()] = $permission;
+                    }
+                }
+            }
+        }
+        $ret=[];
+        foreach ($groups as $group){
+            $groupArray=(object)(array)$group;
+            unset($groupArray->permission);
+            $groupArray->children=array_values($data[$group->name->__toString()]);
+            $ret[]=$groupArray;
+        }
+        return $ret;
     }
 }
