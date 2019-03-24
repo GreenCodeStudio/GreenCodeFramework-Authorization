b884dfeba341e7fb818a4a5234772debce8f2387
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-03-24 17:52:43 +0100

    poprawki

--- js/Controllers/Authorization.js
+++ js/Controllers/Authorization.js
@@ -1,3 +1,5 @@
+import {Ajax} from "../../../Core/js/ajax";
+
 export default class {
     constructor(page, data) {
         page.querySelectorAll('.loginForm').forEach(x => x.addEventListener('submit', async e => {
@@ -14,6 +16,21 @@ export default class {
                 form.querySelector('.error').classList.remove('hidden');
             }
         }));
+        page.querySelectorAll('.registerForm').forEach(x => x.addEventListener('submit', async e => {
+            e.preventDefault();
+            try {
+                var form = document.querySelector('.registerForm');
+                let data = await Ajax.User.register(form.mail.value, form.password.value, form.password2.value);
+                document.location = '/';
+            } catch (ex) {
+                if (ex.type === "Authorization\\Exceptions\\BadAuthorizationException")
+                    form.querySelector('.error').textContent = 'Zły login lub hasło';
+                else
+                    form.querySelector('.error').textContent = 'Błąd';
+                form.querySelector('.error').classList.remove('hidden');
+            }
+        }));
+
         page.querySelectorAll('input').forEach(x => x.addEventListener('input', e => {
             if (e.target.value == "")
                 e.target.classList.remove('notEmpty');
